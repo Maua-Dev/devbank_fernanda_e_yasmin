b9fdf1af94b703584dc269ad60429008f1d8f2b9
febeatriz <febiatx@gmail.com>
2023-08-16 19:59:46 -0300

    agency, account and  balance criated

--- src/app/entities/user.py
+++ src/app/entities/user.py
@@ -9,3 +9,21 @@ class User:
     current_balance: float
 
 def __init__(self, name:srt= None, agency: str=None, account: str=None, current_balance: float=None):
+    validation_name = self.validate_name(name)
+    if validation_name[0] is False:
+        raise ParamNotValidated("name", validation_name[1])
+    self.name = name
+
+    validation_agency = self.validate_agency(agency)
+    if validation_agency[0] is False:
+        raise ParamNotValidated("agency", validation_agency[1])
+    self.agency = agency
+
+    validation_account = self.validation_account(account)
+    if validation_account[0] is False:
+        raise ParamNotValidated("account", validation_account[1])
+    self.account = account
+
+    validation_current_balance = self.validation_current_balance(current_balance)
+    if validation_current_balance[0] is False:
+        raise ParamNotValidated("current balance", validation_current_balance[1])
