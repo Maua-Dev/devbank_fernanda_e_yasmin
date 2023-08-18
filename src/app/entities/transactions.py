8960f985a7bb57e5f1521ef232ed9eb5a13f4cc0
yasmin bonilha <bonilhayaya@gmail.com>
2023-08-18 09:06:01 -0300

    tentativa de criação de entidades

---
+++ src/app/entities/transactions.py
@@ -0,0 +1,50 @@
+from typing import Tuple
+from src.app.enums.transaction_type_enum import TransactionTypeEnum
+from ..errors.entity_errors import ParamNotValidated
+import datetime
+
+class Transactions:
+    transaction_type = TransactionTypeEnum
+    current_balance = float
+    timestamp = float
+
+def __init__(self, transaction_type= None, current_balance= 0, timestamp=0):
+    validation_transaction_type = self.validate_transaction_type(transaction_type)
+    if transaction_type[0] is False:
+        raise ParamNotValidated("transaction_type", validate_transaction_type[1])
+    self.transaction_type = transaction_type
+
+    validation_current_balance = self.validate_current_balance(current_balance)
+    if current_balance[0] is False:
+        raise ParamNotValidated("current_balance", validate_current_balance[1])
+    self.current_balance = current_balance
+
+    validation_timestamp = self.validate_timestamp(timestamp)
+    if timestamp[0] is False:
+        raise ParamNotValidated("timestamp", validate_timestamp[1])
+    self.timestamp = timestamp
+
+@staticmethod
+def validate_transaction_type(transaction_type: TransactionTypeEnum) -> Tuple[bool, str]:
+    if transaction_type is None:
+        return (False, "transaction type is required")
+    if type(transaction_type) != TransactionTypeEnum:
+        return (False, "transaction_type must be a TransactionTypeEnum")
+    return (True, "")
+
+@staticmethod
+def validate_current_balance(current_balance: float) -> Tuple[bool, str]:
+    if type(current_balance) != float:
+        return (False, "current balance must be a float")
+    if current_balance < 0:
+        return (False, "current balance must be positive")
+    return (True, "")
+
+@staticmethod
+def validate_timestamp(timestamp: float) -> Tuple[bool, str]:
+    if timestamp is None:
+        return (False, "timestamp must be valid")
+    if timestamp != float:
+        return (False, "timestamp must be a float")
+    return (True, "")
+
--- src/app/entities/user.py
+++ src/app/entities/user.py
@@ -11,19 +11,57 @@ class User:
 def __init__(self, name:str= None, agency: str=None, account: str=None, current_balance: float=None):
     validation_name = self.validate_name(name)
     if validation_name[0] is False:
-        raise ParamNotValidated("name", validation_name[1])
+        raise ParamNotValidated("name", validate_name[1])
     self.name = name
 
     validation_agency = self.validate_agency(agency)
     if validation_agency[0] is False:
-        raise ParamNotValidated("agency", validation_agency[1])
+        raise ParamNotValidated("agency", validate_agency[1])
     self.agency = agency
 
-    validation_account = self.validation_account(account)
+    validation_account = self.validate_account(account)
     if validation_account[0] is False:
-        raise ParamNotValidated("account", validation_account[1])
+        raise ParamNotValidated("account", validate_account[1])
     self.account = account
 
-    validation_current_balance = self.validation_current_balance(current_balance)
+    validation_current_balance = self.validate_current_balance(current_balance)
     if validation_current_balance[0] is False:
-        raise ParamNotValidated("current balance", validation_current_balance[1])
+        raise ParamNotValidated("current balance", validate_current_balance[1])
+    
+    @staticmethod
+    def validate_name(name: str) -> Tuple[bool, str]:
+        if name is None:
+            return (False, "Name is required")
+        if type(name) != str:
+            return (False, "Name must be a str")
+        if len(name) < 3:
+            return (False, "Name must be at least 3 characters long")
+        return (True, "")
+        
+    @staticmethod
+    def validate_agency(agency: str) -> Tuple[bool, str]:
+        if str is None:
+            return (False, "Agency is required")
+        if type(agency) != str:
+            return (False, "Agency must be a str")
+        if len(agency) < 4:
+            return (False, "Agency must be a four digit number")
+        return (True, "")
+    
+    @staticmethod
+    def validate_account(account: str) -> Tuple[bool, str]:
+        if account is None:
+            return (False, "Account is required")
+        if type(account) != str:
+            return (False, "Account must be a str")
+        if len(account) != 7:
+            return (False, "Account must have a valid format")
+        return (True, "")
+    
+    @staticmethod
+    def validate_current_balance(current_balance: float) -> Tuple[bool, str]:
+        if current_balance < 0:
+            return (False, "Current balance must be positive")
+        if type(current_balance) != float:
+            return (False, "Current balance must be a float")
+        return (True, "")
