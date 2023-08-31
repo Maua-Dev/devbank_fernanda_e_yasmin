ab60f3d93ff376b5f13fb5bea78e53277403d66d
febeatriz <febiatx@gmail.com>
2023-08-31 18:05:53 -0300

    transaction mock

--- src/app/repo/transaction_repository_mock.py
+++ src/app/repo/transaction_repository_mock.py
@@ -1,28 +1,25 @@
-from typing import Dict, Optional, List
-
-from ..entities.transactions import Transactions
-from ..repo.transaction_repo_interface import ITransactionsRepository
-from ..enums.transaction_type_enum import TRANSACTION_TYPE_ENUM
-from ..entities.user import User
-from ..repo.user_repository_mock import UserRepositoryMock
-import time
+from typing import List
+from src.app.repo.transaction_repo_interface import ITransactionsRepository
+from src.app.enums.transaction_type_enum import TRANSACTION_TYPE
 
 
 class TransactionRepositoryMock(ITransactionsRepository):
-    transactions: Dict[int, Transactions]
-    
-    def __init__(self):
-        user = UserRepositoryMock.get_user()
+    transaction : Transaction
 
-        self.transactions = {
-            1: Transactions(transaction_type= TRANSACTION_TYPE_ENUM.WITHDRAW, current_balance= user.current_balance, timestamp= time.time()),
-            2: Transactions(transaction_type= TRANSACTION_TYPE_ENUM.DEPOSIT, current_balance= user.current_balance, timestamp= time.time()),
-        }
-        
-    def get_all_items(self) -> List[Transactions]:
-        return self.items.values()
-    
-    def get_transaction_type(self) -> List[Transactions]:
-        return self.transactions[1].transaction_type, self.transactions[2].transaction_type
+    def __ini__(self):
+        self.transaction=Transaction()
+
+    def create_transaction(self, transaction: Transaction = None) -> List[str, float]:
+        if transaction.type == TRANSACTION_TYPE.DEPOSIT:
+            transaction.current_balance = transaction.current_balance + transaction.value
+        elif transaction.type == TRANSACTION_TYPE.WITHDRAW:
+            transaction.current_balance = transaction.current_balance - transaction.value
+
+        transaction = Transaction(
+            type_transactions=transaction.type,
+            value=transaction.value,
+            current_balance=transaction.current_balance
+        )
+
     
     
