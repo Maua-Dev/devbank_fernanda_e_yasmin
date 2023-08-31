0030acb2a0c6e7e4b712508b86f32f565f417349
yasmin bonilha <bonilhayaya@gmail.com>
2023-08-31 16:24:49 -0300

    vai

--- src/app/environments.py
+++ src/app/environments.py
@@ -35,10 +35,10 @@ class Environments:
         self.stage = STAGE[os.environ.get("STAGE")]
 
     @staticmethod
-    def get_item_repo() -> IItemRepository:
+    def get_user_repo() -> IItemRepository:
         if Environments.get_envs().stage == STAGE.TEST:
-            from .repo.item_repository_mock import ItemRepositoryMock
-            return ItemRepositoryMock
+            from .repo.user_repository_mock import UserRepositoryMock
+            return UserRepositoryMock
         # use "elif" conditional to add other stages
         else:
             raise EnvironmentNotFound("STAGE")
